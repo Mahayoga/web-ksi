3fe26c5e722068f06ebdfeb49e02b4c9f60dec6f
Mahayoga <myoga.bahtiar@gmail.com>
2025-02-09 17:51:36 +0700

    Add migrasi tabel jabatan dan pangkat

---
+++ database/migrations/2025_02_07_061921_create_pangkat_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pangkat', function (Blueprint $table) {
+            $table->uuid('id_pangkat')->unique();
+            $table->string('nama_pangkat');
+            $table->string('golongan');
+            $table->string('angka_kredit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pangkat');
+    }
+};
